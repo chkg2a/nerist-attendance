79f66f508f7f6f9451579ed55fc5986945e6a523
chkg2a <xshalltearvamps@gmail.com>
2025-03-07 18:08:43 +0530

    2025-03-07 18:08:43

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 // App.jsx
 import { useState, useEffect } from "react";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import "./App.css";
 import SubjectForm from "./components/SubjectForm";
 import Results from "./components/Results";
@@ -188,7 +189,7 @@ function App() {
       <div className="app-container">
         <div className="app-content">
           <Header />
-
+          <SpeedInsights/>
           <div className="form-container">
             <h2 className="section-title">Your Subjects</h2>
 
